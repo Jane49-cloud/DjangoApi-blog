6bc53e76d1caaa4b27e79b3922aea87737d25638
Jane49-cloud <janendirangu49@gmail.com>
2024-07-22 23:10:45 +0300

    configure build

---
+++ build_files.sh
@@ -0,0 +1,13 @@
+echo "building project packages..."
+python3 -m pip install -r requirements.txt
+
+echo "migrating database..."
+
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+
+
+echo "collecting static files..."
+python3 manage.py collectstatic --noinput --clear
+ echo "BUILD END"
